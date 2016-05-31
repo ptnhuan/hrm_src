7f825fbd07ca64961e532097f03b413f74950cc4
Nhathv <hovannhat15@gmail.com>
2016-05-31 14:01:34 +0700

    a2
    aaa

--- resources/views/vendor/laravel-authentication-acl/admin/layouts/navbar.blade.php
+++ resources/views/vendor/laravel-authentication-acl/admin/layouts/navbar.blade.php
@@ -29,18 +29,7 @@ $less->compileFile(public_path() . '/packages/jacopo/laravel-authentication-acl/
                         <li class="header">You have 4 messages</li>
                         <li>
                             <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 200px;"><ul class="menu" style="/*overflow: hidden*/; width: 100%; height: 200px;">
-                                    <li>
-                                        <a href="#">
-                                            <div class="pull-left">
-                                                <img src="images/user2-160x160.jpg" class="img-circle" alt="User Image">
-                                            </div>
-                                            <h4>
-                                                Support Team
-                                                <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                                            </h4>
-                                            <p>Why not buy a new awesome theme?</p>
-                                        </a>
-                                    </li>
+                                   
                                     <li>
                                         <a href="#">
                                             <div class="pull-left">
