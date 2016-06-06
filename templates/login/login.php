2850ac6f33bdd61d3dec92764c815348369f3dd7
TheLocNguyen <pykun6@gmail.com>
2016-06-06 10:32:43 +0700

    commit
    commit

---
+++ templates/login/login.php
@@ -0,0 +1,60 @@
+
+<div class="container">
+    <div class="loginbox">
+        <span class="site-image"></span>
+        <div class="innerheading">
+            <h1>
+                Login
+            </h1>
+        </div>
+        <form method="post" action="" onsubmit="" id="" style="padding-bottom: 25px;">
+            <div class="aspNetHidden">
+                <input type="hidden" name="" id="" value="">
+                <input type="hidden" name="" id="" value="">
+                <input type="hidden" name="" id="" value="">
+            </div>
+            <div class="aspNetHidden">
+
+                <input type="hidden" name="" id="" value="">
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <div id="uxHelperInfoBox" class="helpbox">
+                        <p>
+                            <strong>
+                            </strong>
+                        </p>
+                        <p>
+                            <strong>Having Trouble?</strong>
+                            <br>
+                            For help with any technical issues or questions, call us on:
+                        </p>
+                        <p class="phone">
+                            <strong>0905 005 2919</strong>
+                        </p>
+                        <p>
+                            Or visit our <a class="" href="#">FAQ</a>
+                        </p>
+                    </div>
+
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="login-form">
+                        <label>
+                            Email</label><br>
+                        <input name="UserName" type="text" id="UserName" tabindex="1" class="textEntry" autofocus="autofocus">
+                        <span id="RequiredFieldValidator5" title="User Name is required." class="failureNotification" style="visibility: hidden;">*</span>
+                        <label>
+                            Password</label><br>
+                        <input name="Password" type="password" id="Password" tabindex="2" class="passwordEntry">
+                        <span id="RequiredFieldValidator6" title="Password is required." class="failureNotification" style="visibility: hidden;">*</span><br>
+                        <br>
+                        <a class="fgtn" href="#" tabindex="4">Forgotten password?</a>
+                        <input type="submit" name="LoginButton" value="Submit" onclick="" id="" tabindex="3" class="btn">
+                    </div>
+                </div>
+            </div>
+        </form>
+
+    </div>
+</div>
